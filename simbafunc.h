e1f695e24ea7ba486b545e8552e54dd7589efed6
Rogerio Albert <rogerioalbertmozer@hotmail.com>
2020-09-27 18:00:01 -0300

    Fase 1 TAD SimBa

---
+++ simbafunc.h
@@ -0,0 +1,16 @@
+// TAD of SimBa's functions
+
+float FieldGoalMade(int shotsmade, int totalgames);
+float FieldGoalAtp(int shots, int totalgames);
+float FieldGoalPercent(int shotsmade, int fga);
+float ThreePointers(int threepointmade, int totalgames);
+float ThreePointAtp(int threepoint, int totalgames);
+float ThreePointPercent(int threepointmade, int tpa);
+float FreeThrowsMade(int ftmade, int totalgames);
+float FreeThrowAtp(int fts, int totalgames);
+float FreeThrowPercent( int ftmade,int fta);
+float Rebounds(int reb, int totalgames);
+float Turnovers(int turnov,int totalgames);
+float Steals(int steal, int totalgames);
+float Blocks(int block, int totalgames);
+float TeamFouls(int fouls, int totalgames);
