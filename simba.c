868b395c080b87921d0c254c7e94326ca484f36d
Rogerio Albert <rogerioalbertmozer@hotmail.com>
2020-11-23 16:02:40 -0300

    Arquivo principal SimBa

--- simba.c
+++ simba.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include "simbafunc.h"
+
 typedef struct{
 	char nome[22];
 	FILE* roster;
@@ -134,8 +137,13 @@ void gameTime(franquia time1, franquia time2){
 	//char s, c;
 	//int i = 0, pos = 1, enter=0;
 	int game = 1,winA = 0,winH = 0,score1,score2;
+	int assists1, assists2,rebote1,rebote2,turno1,turno2,steals1,steals2;
+	int blocks1,blocks2,fouls1,fouls2;
+	int totalPT1 = 0,totalPT2 = 0,totalAS1 = 0,totalAS2 = 0,totalREB1 = 0,totalREB2 = 0;
+	int totalTURN1 = 0,totalTURN2 = 0,totalSTL1 = 0,totalSTL2 = 0,totalBLK1 = 0,totalBLK2 = 0,totalFL1 = 0,totalFL2 = 0;
 	
 	srand(time(0));
+	
 	while(winA < 4 || winH < 4){
 		printf("===========================================================================================");
 		printf("\n");
@@ -159,14 +167,66 @@ void gameTime(franquia time1, franquia time2){
 		}else{
 			winH++;
 		}
+		assists1 = 24 + pow(-1,rand())*(rand() % (4 + 1 - 0) + 0);
+		assists2 = 24 + pow(-1,rand())*(rand() % (4 + 1 - 0) + 0);
+		rebote1 = 45 + pow(-1,rand())*(rand() % (6 + 1 - 0) + 0);
+		rebote2 = 45 + pow(-1,rand())*(rand() % (6 + 1 - 0) + 0);
+		turno1 = 15 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		turno2 = 15 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		steals1 = 8 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		steals2 = 8 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		blocks1 = 5 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		blocks2 = 5 + pow(-1,rand())*(rand() % (2 + 1 - 0) + 0);
+		fouls1 = 21 + pow(-1,rand())*(rand() % (3 + 1 - 0) + 0);
+		fouls2 = 21 + pow(-1,rand())*(rand() % (3 + 1 - 0) + 0);
+		
+		printf("\tASSISTS1 = %d\t\t ASSISTS2 = %d\n\n",assists1,assists2);
+		printf("\tREBOUNDS1 = %d\t\t REBOUNDS2 = %d\n\n",rebote1,rebote2);
+		printf("\tTURNOVER1 = %d\t\t TURNOVER2 = %d\n\n",turno1,turno2);
+		printf("\tSTEALS1 = %d\t\t STEALS2 = %d\n\n",steals1,steals2);
+		printf("\tBLOCKS1 = %d\t\t BLOCKS2 = %d\n\n",blocks1,blocks2);
+		printf("\tFOULS1 = %d\t\t FOULS2 = %d\n\n",fouls1,fouls2);
+		
+		totalPT1 += score1;
+		totalPT2 += score2; 
+		totalAS1 += assists1; 
+		totalAS2 += assists2; 
+		totalREB1 += rebote1; 
+		totalREB2 += rebote2; 
+		totalTURN1 += turno1; 
+		totalTURN2 += turno2; 
+		totalSTL1 += steals1; 
+		totalSTL2 += steals2; 
+		totalBLK1 += blocks1; 
+		totalBLK2 += blocks2; 
+		totalFL1 += fouls1; 
+		totalFL2 += fouls2; 
+		 
+		
 		if(winA == 4){
 			printf("===========================================================================================\n");
 			printf("\t\t%s GANHOU (%d-%d)\n",time2.nome,winA,winH);
+			
+			printf("\tPPG1: %.1f\t\t PPG2: %.1f\n\n",(totalPT1*1.0)/game,(totalPT2*1.0)/game);
+			printf("\tAPG1: %.1f\t\t APG2: %.1f\n\n",Assists(totalAS1,game),Assists(totalAS2,game));
+			printf("\tREB1: %.1f\t\t REB2: %.1f\n\n",Rebounds(totalREB1,game),Rebounds(totalREB2,game));
+			printf("\tTO1: %.1f\t\t TO2: %.1f\n\n",Turnovers(totalTURN1,game),Turnovers(totalTURN2,game));
+			printf("\tSTL1: %.1f\t\t STL2: %.1f\n\n",Steals(totalSTL1,game),Steals(totalSTL2,game));
+			printf("\tBLK1: %.1f\t\t BLK2: %.1f\n\n",Blocks(totalBLK1,game),Blocks(totalBLK2,game));
+			printf("\tPF1: %.1f\t\t PF2: %.1f\n\n",TeamFouls(totalFL1,game),TeamFouls(totalFL2,game));
 			printf("===========================================================================================\n");
 			break;
 		}else if(winH == 4){
 			printf("===========================================================================================\n");
 			printf("\t\t%s GANHOU (%d-%d)\n",time1.nome,winH,winA);
+			
+			printf("\tPPG1: %.1f\t\t PPG2: %.1f\n\n",(totalPT1*1.0)/game,(totalPT2*1.0)/game);
+			printf("\tAPG1: %.1f\t\t APG2: %.1f\n\n",Assists(totalAS1,game),Assists(totalAS2,game));
+			printf("\tREB1: %.1f\t\t REB2: %.1f\n\n",Rebounds(totalREB1,game),Rebounds(totalREB2,game));
+			printf("\tTO1: %.1f\t\t TO2: %.1f\n\n",Turnovers(totalTURN1,game),Turnovers(totalTURN2,game));
+			printf("\tSTL1: %.1f\t\t STL2: %.1f\n\n",Steals(totalSTL1,game),Steals(totalSTL2,game));
+			printf("\tBLK1: %.1f\t\t BLK2: %.1f\n\n",Blocks(totalBLK1,game),Blocks(totalBLK2,game));
+			printf("\tPF1: %.1f\t\t PF2: %.1f\n\n",TeamFouls(totalFL1,game),TeamFouls(totalFL2,game));
 			printf("===========================================================================================\n");
 			break;	
 		}
@@ -174,9 +234,24 @@ void gameTime(franquia time1, franquia time2){
 		
 	}
 }
+// 54.7field goals attempt per game
+// 28.7 field goals made per game
+// == 52.4% shots made
+// 34.1 3-point attempt
+// 12.2 3-point made
+// == 35.8% 3-point
+// 23.1 free throw attempt
+// 17.9 free throw made
+// 77.3% free throw made
+// 44.8 reb per game - finished
+// 24.4 assists per game - finished
+// 14.5 turnover per game - finished
+// 7.6 steals per game - finished
+// 4.9 blocks per game - finished
+// 20.8 team fouls per game - finished
 
-
-
+// caracteristica geral individual: Dividir jogo a jogo pra cada jogador
+// usar recursividade + arquivo pra cada jogador
 
 
 
